5918e7589f3f56c4463bc6e81ef01a7dd04f4033
sarah-morgenstein <sarah.morgenstein@tri-c.edu>
2022-10-18 09:43:21 -0400

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+// I understand how for loops work. The first placement in the parenthesis is where the for loop starts, the second is the maximum number, and the third placement is i++. In the brackets is the code you would place for the length of that loop. If there are no numbers, then a for loop keeps looping until it is false.
+
+// for(establish starting point of index and index variable; any stopping condition which returns true/false; increment index)
+for (i = 0; i <= 19; i++) {
+  console.log("Regular Loop: the current index is " + i);
+}
+
+// infinite loop example
+// for (i = 0; i < i + 1; i++) {
+//   console.log("Infinite Loop: the current index is " + i);
+// }
